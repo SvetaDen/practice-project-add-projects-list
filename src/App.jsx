906e1aabfe6132dc8377a80d2e03079f5a0bde40
Sveta Denisyuk <svetlana.denisyuk@gmail.com>
2024-04-27 21:18:28 +0300

    to add newProject and after save return to the page for add new project

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,40 @@ import NoProjectSelected from "./components/NoProjectSelected";
 import {useState} from "react";
 
 function App() {
-    const[projectsState,setProjectsState]=useState({
-        selectedProject: undefined,
-        projects:[]
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: [],
     });
 
-    function handleStartAddProject(){
-     setProjectsState(prevState=>{
-         return{
-             ...prevState,
-             selectedProject: null
-         };
-     });
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null
+            };
+        });
     }
+
+    function handleAddProject(projectData) {
+        setProjectsState(prevState => {
+            const projectId = Math.random();
+            const newProject = {
+                ...projectData,
+                id: projectId,
+            };
+            return {
+                ...prevState,
+                selectedProjectId:projectId,
+                projects: [...prevState.projects, newProject]
+            };
+        });
+    }
+
     let content;
-    if(projectsState.selectedProject === null){
-        content = <NewProject/>
-    } else if(projectsState.selectedProject === undefined){
+
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject onAdd={handleAddProject}/>
+    } else if (projectsState.selectedProjectId === undefined) {
         content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
     }
 
